ec311de91dc0a42d7144df34f0fed8ff51321abd
Sagar Panwar <85165483+sgrpnwr@users.noreply.github.com>
2021-06-11 23:44:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+#number_array
+var numbers=document.querySelectorAll(".calc-numbers .number-btn");
+numbers[0]=7;
+numbers[1]=8;
+numbers[2]=9;
+numbers[3]=4;
+numbers[4]=5;
+numbers[5]=6;
+numbers[6]=1;
+numbers[7]=2;
+numbers[8]=3;
+numbers[9]=0;
+numbers[10]=".";
